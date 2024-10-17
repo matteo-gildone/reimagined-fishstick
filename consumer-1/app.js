b23e75c9a6324d68918d9f5f7f242cbef105f270
Matteo Gildone <matteo.gildone@springernature.com>
2024-10-17 13:52:34 +0100

    feat[DSS]: working examples

---
+++ consumer-1/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const hbs = require('hbs');
+const cookieParser = require('cookie-parser');
+const {promisify} = require('util');
+const path = require('path');
+const axios = require("axios");
+const app = express();
+const port = 3003;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res, next) => {
+    try {
+        const headerParams = {isProduct: true, isLegal: true};
+        const headerResponse = await axios.get('http://localhost:3001/header', {params: headerParams});
+        const journalListAPIResponse = await axios.get('http://localhost:3002/journal-list');
+        const journalListResponse = await axios.get('http://localhost:3001/journal-card-list', {
+                params: {
+                    journals: [...journalListAPIResponse.data]
+
+                }
+            })
+        ;
+        res.render('index', {
+            title: 'Homepage',
+            headerPlaceholder: headerResponse.data,
+            journalListPlaceholder: journalListResponse.data
+        });
+    } catch (err) {
+        return new hbs.SafeString(err.message, {});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
--- design-system-service/app.js
+++ design-system-service/app.js
@@ -5,13 +5,78 @@ const port = 3001;
 
 hbs.registerPartials(__dirname + '/eds');
 
+const journalCardMapping = {
+    "publishingModel": "Publishing model",
+    "impactFactor": "Impact factor",
+    "download": "Download",
+    "submission": "Submission to first decision (median)"
+};
+
+const createMapping = list => {
+    return list.map(item => {
+        const {title, summary} = item;
+        const metadata = Object.keys(item)
+            .map(key => {
+                return journalCardMapping[key] ? {label: journalCardMapping[key], text: item[key]} : null;
+            })
+            .filter(item => item !== null);
+        return {
+            title, summary, metadata
+        }
+    });
+
+}
+
 app.get('/', (req, res) => {
     res.send('Design system service');
 });
 
 app.get('/header', (req, res) => {
-    const template = hbs.compile('{{> header/headerTest}}')
-    res.send(template({variable: 'jello world!'}));
+    const {query} = req;
+    const productLinks = [{
+            url: "#",
+            label: "Product"
+        },
+        {
+            url: "#",
+            label: "Another product"
+        }];
+
+    const legalLinks = [{
+        url: "#",
+        label: "Legal"
+    }];
+
+    const featureLinks = [{
+            url: "#",
+            label: "Feature"
+        },
+        {
+            url: "#",
+            label: "Another feature"
+        }];
+    const data = {}
+    data.links = [];
+    if (query.isProduct) {
+        data.links = data.links.concat(productLinks);
+    }
+
+    if (query.isLegal) {
+        data.links = data.links.concat(legalLinks);
+    }
+
+    if (query.isFeature) {
+        data.links = data.links.concat(featureLinks);
+    }
+
+    const template = hbs.compile('{{> header/header}}')
+    res.send(template(data));
+});
+
+app.get('/journal-card-list', (req, res) => {
+    const {query} = req;
+    const template = hbs.compile('{{#each .}}{{> card/card}}{{/each}}')
+    res.send(template(createMapping(query.journals)));
 });
 
 app.listen(port, () => {
